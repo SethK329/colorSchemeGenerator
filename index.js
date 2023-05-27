3d51442f65bab05de7386174fab2d1fd238b6077
Seth Kuhn <107776853+SethK329@users.noreply.github.com>
2023-05-26 22:19:12 -0700

    initial commit, lots to do still
    This version has a constructor producing the html from the api object. Next up is to allow for choosing the color format and dynamically rendering that. Then also allowing for one click copying of color values.

---
+++ index.js
@@ -0,0 +1,66 @@
+
+const modeSelect = document.getElementById("modeSelect")
+const colorInput = document.getElementById("colorInput")
+const colorContainer = document.getElementById("colorContainer")
+const colorSpace = document.getElementById("colorSpace")
+let dataArr = []
+
+// this event listener provides a visual change to show what hex value is currently selected by the input
+document.getElementById("colorInput").addEventListener("change",  e=>{
+    document.getElementById("colorInputLabel").innerText = `Current color: ${colorInput.value}`
+})
+
+// this triggers the series of functions that produce the color scheme
+document.getElementById("getScheme").addEventListener("click",event=>{
+    getColorScheme()
+})
+
+// removes hex from color value for use in api query string
+function cleanHexCode(){
+    let hexValue = colorInput.value.slice(1,colorInput.value.length)
+    return hexValue
+}
+
+// calls api and pushes returned object into an array for use in a constructor
+function getColorScheme(){
+    fetch(`https://www.thecolorapi.com/scheme?hex=${cleanHexCode()}&format=json&mode=${modeSelect.value}&count=5`)
+    .then(res=>res.json())
+    .then(data=> {
+        console.log(data)
+        dataArr.unshift(data)
+        console.log(dataArr)
+        console.log(colorSpace.value)
+        render()
+    })
+}
+
+// prints html produced by the constructor into the DOM
+function render(){
+    colorContainer.innerHTML = new ColorScheme(dataArr[0]).getColorContainerHTML(colorSpace.value)
+}
+
+// constructor for turning object returned by api into html visual. It is overkill right now,
+//  but would make it easier to use all of the data returned by the api and produce a sort of color pallete dashboard
+class ColorScheme{
+    constructor(data){
+        Object.assign(this, data)
+    }
+    getColorContainerHTML(format) {
+        const {colors} = this
+        console.log(format)
+        colorContainer.innerHTML =""
+        let colorArr = colors
+        let palletteHTML = ''
+        colorArr.forEach(color =>{
+            palletteHTML +=`
+             <div class = "color-panel">
+                <p class ="renderedColor">${color.hex.value}</p>
+                <img src="${color.image.bare}" class="color-sample"/>
+            </div>
+             `
+
+        })
+        return palletteHTML
+    }
+}
+getColorScheme()
