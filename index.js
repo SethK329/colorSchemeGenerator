ed3f453f75ced98d5ae107d061c604c163ac168f
Seth Kuhn <107776853+SethK329@users.noreply.github.com>
2023-06-05 13:06:54 -0700

    JS functionality done.

--- index.js
+++ index.js
@@ -3,79 +3,81 @@ const modeSelect = document.getElementById("modeSelect")
 const colorInput = document.getElementById("colorInput")
 const colorContainer = document.getElementById("colorContainer")
 const colorSpace = document.getElementById("colorSpace")
+const searchedColor = document.getElementById("searchedColor")
 const currentColor = document.getElementById("currentColor")
 let dataArr = []
 let colorIndex = 0
 
-//shows what hex value is currently selected
-colorInput.addEventListener("change", showCurrentColor)
 
-// changes which color format is displayed
-colorSpace.addEventListener("change", e=>{
-    for(let i = 0; i<dataArr.length; i++){
-        dataArr[i].colorFormat = colorSpace.value
-    }
-    getCurrentColor()
-})
+// EVENT LISTENERS
+    //shows in text what hex value is currently selected in color input
+    colorInput.addEventListener("change",()=>{currentColor.innerText=`Current: ${colorInput.value}` } )
 
-// this triggers the series of functions that produce the color scheme
-document.getElementById("getScheme").addEventListener("click", e=>{
-    getColorScheme()
-})
+    // changes which color format is displayed
+    colorSpace.addEventListener("change", e=>{
+        for(let i = 0; i<dataArr.length; i++){
+            dataArr[i].colorFormat = colorSpace.value
+        }
+        render(colorIndex)
+    })
 
-// copies color info from data attribute set by constructor
-colorContainer.addEventListener("click", e=> {
-    navigator.clipboard.writeText(e.target.dataset.color);
-    console.log("copied")
-})
+    //on button click this triggers the series of functions that produce the color scheme
+    document.getElementById("getScheme").addEventListener("click", getColorScheme)
 
-// finds index for selected color that has been searched and then triggers render on that color
-currentColor.addEventListener("change", getCurrentColor)
+    // copies color info from data attribute set by constructor
+    colorContainer.addEventListener("click", e=> {
+        navigator.clipboard.writeText(e.target.dataset.color);
+    })
 
-// removes hex from color value for use in api query string
-function cleanHexCode(){
-    let hexValue = colorInput.value.slice(1,colorInput.value.length)
-    return hexValue
-}
+    // finds index for selected color that has been searched and then triggers render on that color
+    searchedColor.addEventListener("change", getCurrentColor)
 
-// calls api and pushes returned object into an array for use in a constructor, also add the colorspace value so that when it is changed the displayed data is changed too.
+
+
+// calls api and pushes returned object into an array for use in a constructor, also add the colorspace property to data for later use.
 function getColorScheme(){
     fetch(`https://www.thecolorapi.com/scheme?hex=${cleanHexCode()}&format=json&mode=${modeSelect.value}&count=5`)
     .then(res=>res.json())
     .then(data=> {
         data.colorFormat = colorSpace.value
         dataArr.unshift(data)
-        render(colorIndex)
         console.log(dataArr)
+        render(colorIndex)
+        showSearchedColors()
     })
 
 }
 
 // prints html produced by the constructor into the DOM
 function render(arrayIndex){
-    colorContainer.innerHTML = new ColorScheme(dataArr[arrayIndex]).getColorContainerHTML()
-    showCurrentColor()
+    // fallback for browsers that don't support view transition api
+    if(!document.startViewTransition){
+        colorContainer.innerHTML = new ColorScheme(dataArr[arrayIndex]).getColorContainerHTML()
+    }
+    // new view transition api. Does automatic animations for changes to the DOM. Currently only works in Chrome 111 and Edge 111.
+    document.startViewTransition(()=> colorContainer.innerHTML = new ColorScheme(dataArr[arrayIndex]).getColorContainerHTML()) 
 }
 
-// utility function to display current selected color and  all searched colors
-function showCurrentColor(){
-    currentColor.innerHTML = ""
+// utility function to display searched colors so user can go back and look at pallette again
+function showSearchedColors(){
+    searchedColor.innerHTML = ""
     let allSearchedColors = ""
     for(let i=0; i<dataArr.length; i++){
-        allSearchedColors += `<option value ="${dataArr[i].seed.hex.value}"> ${dataArr[i].seed.hex.value} </option>`
+        allSearchedColors += `<option value ="${dataArr[i]._links.self}"> ${dataArr[i].seed.hex.value} </option>`
     }
-    currentColor.innerHTML = allSearchedColors
+    searchedColor.innerHTML = allSearchedColors
 }    
 
-// function that will run with render() to create an options list with each searched color. Loop through data array to write the HTML into the DOM each time a pallete is requested.
-// Then allow for the selection of that color and re-render from the dataArr.
+// selects the appropriate index from the dataArr for render when looking at a previously searched color and adjusts other options to match the change
 function getCurrentColor(){
-    colorIndex = dataArr.findIndex(i => i.seed.hex.value === currentColor.value)
-    console.log(colorIndex)
+    colorIndex = dataArr.findIndex(i => i._links.self === searchedColor.value)
+    colorInput.value = dataArr[colorIndex].seed.hex.value
+    modeSelect.value = dataArr[colorIndex].mode
     render(colorIndex)
 }
-// constructor for turning object returned by api into html visual. It is overkill right now,
-//  but would make it easier to use all of the data returned by the api and produce a sort of color pallete dashboard
+
+// constructor for turning object returned by api into html visual. It might be overkill right now,
+//  but I imagine it would make it easier to use all of the data returned by the api and produce a sort of color pallete dashboard
 class ColorScheme{
     constructor(data){
         Object.assign(this, data)
@@ -86,7 +88,7 @@ class ColorScheme{
         let palletteHTML = ''    
         colors.forEach(color =>{
             // ternary lets me choose what colorspace to display from the object by checking the option from the dom and then changing the variable
-            let formatVariable = colorFormat === "hex"? color.hex.value : colorFormat === "rgb"? color.rgb.value: colorFormat === "hsl"? color.hsl.value: color.hsv.value;
+            let formatVariable = colorFormat === "hex"? color.hex.value : colorFormat === "rgb"? color.rgb.value: color.hsl.value;
             palletteHTML +=` <div class = "color-panel" data-color ="${formatVariable}">
                                 <p class ="rendered-color" data-color ="${formatVariable}">${formatVariable}</p>
                                 <img src="${color.image.bare}" class="color-sample" data-color ="${formatVariable}"/>
@@ -95,4 +97,11 @@ class ColorScheme{
         return palletteHTML
     }
 }
+
+// removes hex from color value for use in api query string
+function cleanHexCode(){
+    let hexValue = colorInput.value.slice(1,colorInput.value.length)
+    return hexValue
+}
+
 getColorScheme()
